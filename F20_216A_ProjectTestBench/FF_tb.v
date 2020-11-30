293b841c6b4a085e5c5411901aaa9a06feef4998
MrHePro <72667042+MrHePro@users.noreply.github.com>
2020-12-01 00:14:52 +0800

    FF tested
    usage: FF #(WL) FF_DUT(clk, rst, D, Q);

---
+++ F20_216A_ProjectTestBench/FF_tb.v
@@ -0,0 +1,52 @@
+//time sclae for simulation perposes
+`timescale 1 ns/10 ps
+
+module FF_tb();
+
+reg clk, rst;
+reg [25:0] D1;
+reg [18:0] D2;
+wire [25:0] Q1;
+wire [18:0] Q2;
+
+
+FF #(26) FF_DUT1(clk, rst, D1, Q1);
+FF #(19) FF_DUT2(clk, rst, D2, Q2);
+
+always #5 clk=~clk;
+
+initial begin
+    
+    clk = 0;
+    rst = 0;
+
+    @(negedge clk);
+    rst = 1;
+    @(negedge clk);
+    rst = 0;
+
+    @(negedge clk);
+    D1 = 26'h3ffffff;
+    D2 = 19'h7ffff;
+
+    @(negedge clk);
+    D1 = 26'hffffff;
+    D2 = 19'hffff;
+
+    @(negedge clk);
+    D1 = 26'h3ffffff;
+    D2 = 19'h7ffff;
+
+    
+    @(negedge clk);
+    @(negedge clk);
+    @(negedge clk);
+    rst = 1;
+    @(negedge clk);
+    rst = 0;
+
+    $stop();
+
+end
+
+endmodule;
---
+++ Verilog/FF.v
@@ -0,0 +1,13 @@
+module FF(clk, GlobalReset,D,Q);
+
+parameter IN_SIZE = 10;
+input                       clk, GlobalReset;
+input       [IN_SIZE-1:0] D;
+output  reg [IN_SIZE-1:0] Q;
+
+always@(posedge clk) begin
+    if(GlobalReset) Q <= 0;
+    else            Q <= D;
+end
+
+endmodule;
